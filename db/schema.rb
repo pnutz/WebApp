3f9c7c6ea3b980170b4f209835bb121c67e8c5f5
Oscar Hou <oscarhou@gmail.com>
2013-11-29 00:32:08 -0800

    Added the extensions table (For experimenting)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131127052459) do
+ActiveRecord::Schema.define(version: 20131129082721) do
+
+  create_table "extensions", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
