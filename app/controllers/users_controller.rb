92b917c30d42182a3f636e0975f33308272ec959
Oscar Hou <oscarhou@gmail.com>
2013-12-12 00:50:22 -0800

    Add simple User view test and also on user home page folder names link to folder views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
     def index
-        @userList = User.all
+        @users = User.all
     end
 end
--- spec/views/user/index.html.erb_spec.rb
+++ spec/views/user/index.html.erb_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
-describe "user/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "users/index" do
+  before(:each) do
+    assign(:users, [
+      stub_model(User,
+        :email => "testemail@email.com",
+        :encrypted_password => "TESTPASS",
+      ),
+      stub_model(User,
+        :email => "testemail2@email.com",
+        :encrypted_password => "TESTPASS2",
+      )
+    ])
+  end
+
+  it "renders a list of user emails" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "ul>li", :text => "testemail@email.com".to_s, :count => 1
+    assert_select "ul>li", :text => "testemail2@email.com".to_s, :count => 1
+  end
 end
