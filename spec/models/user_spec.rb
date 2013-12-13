68ad9f4ffb137d5243d81b157ff9d95df6553859
oscarhou <oscarhou@gmail.com>
2013-12-13 00:49:38 -0800

    Modified user index page and made simple modifications to user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context "In general" do
+        it "must have an email and password" do
+            user = User.new()
+            user.should_not be_valid
+
+        end
+    end
 end
--- spec/views/user/index.html.erb_spec.rb
+++ spec/views/user/index.html.erb_spec.rb
@@ -6,10 +6,12 @@ describe "users/index" do
       stub_model(User,
         :email => "testemail@email.com",
         :encrypted_password => "TESTPASS",
+        :created_at => Time.new(1111,11,11,11,11,11),
       ),
       stub_model(User,
         :email => "testemail2@email.com",
         :encrypted_password => "TESTPASS2",
+        :created_at => Time.new(1111,11,11,11,11,11),
       )
     ])
   end
@@ -17,7 +19,7 @@ describe "users/index" do
   it "renders a list of user emails" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "ul>li", :text => "testemail@email.com".to_s, :count => 1
-    assert_select "ul>li", :text => "testemail2@email.com".to_s, :count => 1
+    assert_select "tr>td", :text => "testemail@email.com".to_s, :count => 1
+    assert_select "tr>td", :text => "testemail2@email.com".to_s, :count => 1
   end
 end
