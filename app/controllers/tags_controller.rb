2dc0da9b729de43f2802bc26117b4b7bc4f4e620
Oscar Hou <oscarhou@gmail.com>
2014-08-07 21:00:08 -0700

    added delete to tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,10 @@
 class TagsController < ApplicationController
   load_and_authorize_resource
-  before_action :set_type, only: [:create]
 
   # GET /tags/:type/:id
+  # :type - the type of data to associate the tag with
+  #         ie. receipt_item, user, receipt 
+  # :id - the id of the type_item to be associated
   def index
     case params[:type]
     when "user"
@@ -14,41 +16,47 @@ class TagsController < ApplicationController
     else
       # TODO: handle properly
       puts "Received Invalid Parameter"
+      render status: 400 
     end
     @tags = @model.find(params[:id]).tags
   end
 
   # PUT /tags/:type/:id
+  # :type - the type of data to associate the tag with
+  #         ie. receipt_item, user, receipt 
+  # :id - the id of the type_item to be associated
   def create
     if (params[:name] != nil)
-      @tag = Tag.find_or_create_by(name: params[:name])
+      # retrieve type_item from database 
       _type_item = @model.find(params[:id])
-      _item_type.tags<<@tag
-      render status: 200
+      if (_type_item != nil)
+        @tag = Tag.find_or_create_by(name: params[:name])
+        # associate the tag with the item
+        _item_type.tags<<@tag
+        render status: 200
+      end
+      render status: 404
     else
       render status: 500
     end
   end
 
+  # DELETE /tags/:type/:id/:tag_id
+  # :type - the type of data to associate the tag with
+  #         ie. receipt_item, user, receipt 
+  # :id - the id of the type_item 
+  # :tag_id - the id of the tag to be removed from the type_item
   def delete
-  end
-
-  private
-
-  def set_type_all
-    case params[:type]
-    when "receipt"
-      @model = Receipt
-    when "receipt_item"
-      @model = ReceiptItem
+    if (params[:name] != nil)
+      _type_item = @model.find(params[:id])
+      if (_type_item != nil)
+        @tag = Tag.find(params[:tag_id])
+        _type_item.delete(@tag)
+        render status: 200
+      end
+      render status: 404 
     else
-      # TODO: handle properly
-      puts "Received Invalid Parameter"
+      render status: 500
     end
   end
-  # Never trust parameters from the scary internet, only allow the white list through.
-    def tag_params
-      params.require(:tag).permit(:name)
-    end
-
 end
